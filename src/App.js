b88c5bf086cf5c8168dbc8bfd3dae75765de77b3
grvt <r.garvit7@gmail.com>
2023-08-25 00:07:49 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, { useState } from 'react'
-import Accordion from './components/Accordion'
-import "./components/Accordion.css"
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import "./components/Accordion.css";
 
 const App = () => {
-  const arr = [false, false, false, false];
-  const [checkId, setCheckId] = useState(arr);
+  const [checkId, setCheckId] = useState(-1);
 
-  console.log(!checkId[0]);
-  const checker = (str) =>{
-    setCheckId(arr);
-    checkId[str - 1] = true;
-  }
+  const checker = (str) => {
+    setCheckId(Number(str));
+  };
 
   return (
     <div>
-      <div className='container'>
-        <Accordion handler={checker} id="1" active={checkId[0]} title="Accordion1" data1="lorem10" />
-        <Accordion handler={checker} id="2" active={checkId[1]} title="Accordion1" data1="lorem10" />
-        <Accordion handler={checker} id="3" active={checkId[2]} title="Accordion1" data1="lorem10" />
-        <Accordion handler={checker} id="4" active={checkId[3]} title="Accordion1" data1="lorem10" />
+      <div className="container">
+        <Accordion
+          handler={checker}
+          id={1}
+          active={checkId === 1}
+          title="Accordion1"
+          data1="lorem10"
+        />
+        <Accordion
+          handler={checker}
+          id={2}
+          active={checkId === 2}
+          title="Accordion1"
+          data1="lorem10"
+        />
+        <Accordion
+          handler={checker}
+          id={3}
+          active={checkId === 3}
+          title="Accordion1"
+          data1="lorem10"
+        />
+        <Accordion
+          handler={checker}
+          id={4}
+          active={checkId === 4}
+          title="Accordion1"
+          data1="lorem10"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,32 +1,24 @@
 import React, { useState } from "react";
 import "./Accordion.css";
 
-const Accordion = ({
-  title,
-  data1,
-  active,
-  handler,
-  id
-}) => {
-    const [isShowText, setIsShowText] = useState({active});
-    
-    const handleClick = () =>{
-        handler(id);
-        setIsShowText(!isShowText);
-    }
+const Accordion = ({ title, data1, active, handler, id }) => {
+  const handleClick = () => {
+    handler(id);
+  };
 
-    return (
-        <div>
-            <div className="wrapper">
-              <div className={isShowText ? 'titleBorder' : 'headingText'} onClick={handleClick}>
-                <h3 className="title">{title}</h3>
-              </div>
-                <p className={isShowText ? 'paraText' : 'closed'}>
-                    {data1}
-                </p>
-            </div>
+  return (
+    <div>
+      <div className="wrapper">
+        <div
+          className={active ? "titleBorder" : "headingText"}
+          onClick={handleClick}
+        >
+          <h3 className="title">{title}</h3>
         </div>
-    );
+        <p className={active ? "paraText" : "closed"}>{data1}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Accordion;
